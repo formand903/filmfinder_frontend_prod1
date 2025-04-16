a66353b82c3332af86125e9a8a7891dbe43fbaf4
formand903 <formand903@gmail.com>
2025-04-16 12:10:15 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  root: '.', // важно: корень проекта
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
